7f12a1999f47d970ed952cf979cb8672839f030b
Frank Gregor <phranck@cocoanaut.com>
2014-01-20 01:22:41 +0100

    Changed: manuelly merge

--- CNGridView/CNGridView.h
+++ CNGridView/CNGridView.h
@@ -37,7 +37,7 @@ typedef BOOL (^CNGridViewSelectItem)(CNGridViewItem *item);
 
 /**
  `CNGridView` is a (wanna be) replacement for NSCollectionView. It has full delegate and dataSource support with method calls like known from NSTableView/UITableView.
- 
+
  The use of `CNGridView` is just simple as possible.
  */
 
@@ -54,12 +54,12 @@ typedef BOOL (^CNGridViewSelectItem)(CNGridViewItem *item);
 /**
  Property for the receiver's delegate.
  */
-@property (nonatomic, weak) IBOutlet id<CNGridViewDelegate> delegate;
+@property (nonatomic, assign) IBOutlet id<CNGridViewDelegate> delegate;
 
 /**
  Property for the receiver's data source.
  */
-@property (nonatomic, weak) IBOutlet id<CNGridViewDataSource> dataSource;
+@property (nonatomic, assign) IBOutlet id<CNGridViewDataSource> dataSource;
 
 
 
@@ -68,25 +68,25 @@ typedef BOOL (^CNGridViewSelectItem)(CNGridViewItem *item);
 
 /**
  Property for a title of the grid view.
- 
+
  The default value is `nil`.
  */
 @property (nonatomic, strong) NSString *gridViewTitle;
 
 /**
  Property for the background color of the grid view.
- 
+
  This color (or pattern image) will be assigned to the enclosing scroll view. In the phase of initializing `CNGridView` will
- send the enclosing scroll view a `setDrawsBackground` message with `YES` as parameter value. So it's guaranteed the background 
+ send the enclosing scroll view a `setDrawsBackground` message with `YES` as parameter value. So it's guaranteed the background
  will be drawn even if you forgot to set this flag in interface builder.
- 
+
  If you don't use this property, the default value is `[NSColor controlColor]`.
-*/
+ */
 @property (nonatomic, strong) NSColor *backgroundColor;
 
 /**
  Property for setting the elasticity of the enclosing `NSScrollView`.
- 
+
  This property will set and overwrite the values from Interface Builder. There is no horizontal-vertical distinction.
  The default value is `YES`.
 
@@ -98,26 +98,11 @@ typedef BOOL (^CNGridViewSelectItem)(CNGridViewItem *item);
 
 /**
  Property for setting the grid view item size.
- 
+
  You can set this property programmatically to any value you want. On each change of this value `CNGridView` will automatically
  refresh the entire visible grid view with an animation effect.
  */
 @property (nonatomic, assign) NSSize itemSize;
-
-/**
- ...
- */
-@property(nonatomic,strong) IBOutlet NSView* headerView;
-
-/**
- ...
- */
-@property(nonatomic,assign) BOOL useCenterAlignment;
-
-/**
- ...
- */
-- (NSRect)rectForItemAtIndex:(NSUInteger)index;
 
 
 
@@ -139,7 +124,7 @@ typedef BOOL (^CNGridViewSelectItem)(CNGridViewItem *item);
 
 /**
  Returns the number of currently visible items of `CNGridView`.
- 
+
  The returned value of this method is subject to continous variation. It depends on the actual size of its view and will be calculated in realtime.
  */
 - (NSUInteger)numberOfVisibleItems;
@@ -151,16 +136,16 @@ typedef BOOL (^CNGridViewSelectItem)(CNGridViewItem *item);
 
 /**
  Property for setting whether the grid view allows item selection or not.
- 
+
  The default value is `YES`.
  */
 @property (nonatomic, assign) BOOL allowsSelection;
 
 /**
  Property that indicates whether the grid view should allow multiple item selection or not.
- 
+
  If you have this property set to `YES` with actually many selected items, all these items will be unselect on setting `allowsMultipleSelection` to `NO`.
- 
+
  @param YES The grid view allows multiple item selection.
  @param NO  The grid view don't allow multiple item selection.
  */
@@ -168,9 +153,9 @@ typedef BOOL (^CNGridViewSelectItem)(CNGridViewItem *item);
 
 /**
  Property indicates if the mouse drag operation can be used to select multiple items.
- 
+
  If you have this property set to `YES` you must also set `allowsMultipleSelection`
- 
+
  @param YES The grid view allows multiple item selection with mouse drag.
  @param NO  The grid view don't allow multiple item selection with mouse drag.
  */
@@ -189,7 +174,7 @@ typedef BOOL (^CNGridViewSelectItem)(CNGridViewItem *item);
 /**
  `NSMenu` to use when an item or the selected items are right clicked
  */
-@property (nonatomic, weak) IBOutlet NSMenu *itemContextMenu;
+@property (nonatomic, assign) IBOutlet NSMenu *itemContextMenu;
 
 /**
  Selects all the items in the grid
@@ -241,10 +226,7 @@ typedef BOOL (^CNGridViewSelectItem)(CNGridViewItem *item);
 
 - (void)reloadDataAnimated:(BOOL)animated;
 
-
-
-#pragma mark - Animating KVO Changes
-/** @name Animating KVO Changes */
+#pragma mark - animating KVO changes
 
 - (void)insertItemAtIndex:(NSInteger)index animated:(BOOL)animated;
 - (void)insertItemsAtIndexes:(NSIndexSet*)indexes animated:(BOOL)animated;
@@ -253,5 +235,10 @@ typedef BOOL (^CNGridViewSelectItem)(CNGridViewItem *item);
 
 - (void)beginUpdates;
 - (void)endUpdates;
+
+@property(nonatomic,strong) IBOutlet NSView* headerView;
+@property(nonatomic,assign) BOOL useCenterAlignment;
+
+- (NSRect)rectForItemAtIndex:(NSUInteger)index;
 
 @end
